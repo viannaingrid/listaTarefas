e96a9bdd8f95859700432b2c303b0089b844fce2
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-20 16:37:11 -0300

    :wrench: atualizando com npm e o app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../app/components/header/header.component";
+import { TasksComponent } from "../app/components/tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
