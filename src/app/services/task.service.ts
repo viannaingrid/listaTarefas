702581273235aca77cdd18cbee52e00f100ab0ea
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-20 19:15:22 -0300

    :wrench: add Tarefa para conexão com o DB

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Tarefa } from '../../Tarefa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+  private apiUrl = 'http://localhost:3000/tasks';
+
   constructor(private http: HttpClient) { }
+
+  getTasks() : Observable<Tarefa[]>{
+
+  }
 }
