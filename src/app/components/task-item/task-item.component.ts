731a7042656d64f895c49a2c42f3d346730156c4
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-21 12:54:33 -0300

    :wrench: add FontAwesomeModule

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Tarefa } from '../../../Tarefa';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-task-item',
+  standalone: true,
+  imports: [FontAwesomeModule],
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.scss']
 })
