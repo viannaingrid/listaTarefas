edebc7684ca6607d6d79caa9f5640a8abdbf5b15
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-21 14:39:44 -0300

    :bug: corrigido erros de digitação

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,6 @@
 import { Tarefa } from './../../../Tarefa';
-import { Component, Input, output, EventEmitter } from '@angular/core';
-import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
 
@@ -13,9 +13,8 @@ import { CommonModule } from '@angular/common';
 })
 export class TaskItemComponent {
   @Input() tarefa!: Tarefa;
-  @output() onDeleteTask = new EventEmitter<Tarefa>();
-
-  faTimes = faTimes
+  @Output() onDeleteTask = new EventEmitter<Tarefa>();
+  faTimes = faTimes;
 
   onDelete(tarefa: Tarefa) {
     this.onDeleteTask.emit(tarefa);
