98aa578cade08a97e6f568a8fcde100009032d48
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-21 11:09:35 -0300

    :construction: implementando o common module para conectar o db

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { TaskService } from './../../services/task.service';
+import { Component, OnInit } from '@angular/core';
+import { Tarefa } from '../../../Tarefa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
 
+  tarefas: Tarefa[] = [];
 
+  constructor(private taskService:TaskService){}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((dado) => {
+      this.tarefas = dado;
+      console.log(dado);
+    });
+  }
 }
