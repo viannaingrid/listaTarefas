5ae150d180e0c6932787bf585bbedab2192a7b13
Ingrid Vianna <ingrid.vianna1@outlook.com>
2025-01-21 12:57:09 -0300

    :wrench: add cor no X

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Tarefa } from '../../../Tarefa';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.scss']
 })
